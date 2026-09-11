6f14d9b2b12f15986cb32aa5c5e35f2ecb029f1d
rodrigofrrsilva1703-a11y <rodrigo.frr.silva1703@gmail.com>
2026-09-11 10:32:16 -0300

    Força redeploy da correção completa da conferência 625

---
+++ razync/deploy_625_revision_20260911.py
@@ -0,0 +1,6 @@
+"""Marcador de deploy da correção da conferência da empresa 625.
+
+Revisão: aliases *_625, assinatura com conta, detecção dinâmica do cabeçalho
+e Caixa na conta 504.
+"""
+REVISAO_625 = "2026-09-11-conferencia-v4"
