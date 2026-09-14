4f8e4fbde4e560d317424c985ddeec9d2084df84
rodrigofrrsilva1703-a11y <rodrigo.frr.silva1703@gmail.com>
2026-09-14 16:43:47 -0300

    Força redeploy do filtro de saldo BB 626

---
+++ razync/deploy_sync_626_saldo.py
@@ -0,0 +1,7 @@
+"""Marcador de deploy da empresa 626.
+
+Força o Streamlit Cloud a carregar a versão que remove linhas de saldo final do
+Banco do Brasil mesmo quando o OCR distorce o código 999 ou espaça S A L D O.
+"""
+
+DEPLOY_626_BB_SALDO = "2026-09-14T16:43-03:00"
